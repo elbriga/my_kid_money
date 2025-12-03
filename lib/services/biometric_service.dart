1e52a7e9ed04004c35a1318b56a7f8af45e98e39
elbriga <gabrielortizlour@gmail.com>
2025-12-03 20:39:21 -0300

    desabilitar a biometria

--- lib/services/biometric_service.dart
+++ lib/services/biometric_service.dart
@@ -1,10 +1,12 @@
 import 'package:local_auth/local_auth.dart';
 
 class BiometricService {
+  static final _disabled = 1;
   static final _auth = LocalAuthentication();
 
   static Future<bool> authenticate(String msg) async {
-    return true; // Para funcionar no emulador
+    if (_disabled == 1) return true; // Para funcionar no emulador
+
     try {
       final ok = await _auth.authenticate(
         localizedReason: msg,
