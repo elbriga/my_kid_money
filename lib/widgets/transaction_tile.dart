a8b188b3740898b284429669f5366d20bc94c3f1
elbriga <gabrielortizlour@gmail.com>
2025-12-04 15:25:55 -0300

    format data

--- lib/widgets/transaction_tile.dart
+++ lib/widgets/transaction_tile.dart
@@ -13,6 +13,16 @@ class TransactionTile extends StatelessWidget {
         ? t.description
         : (sign == '+' ? 'Depósito' : 'Saque');
 
+    // Format date - should use de intl package!
+    String data = t.timestamp.toString().split('.')[0];
+    String hora = data.split(' ')[1];
+    data = data.split(' ')[0];
+    String year = data.split('-')[0];
+    String month = data.split('-')[1];
+    String day = data.split('-')[2];
+
+    data = "$day/$month/$year $hora";
+
     return ListTile(
       leading: CircleAvatar(child: Text(sign)),
       title: Text(
@@ -20,10 +30,7 @@ class TransactionTile extends StatelessWidget {
         style: TextStyle(color: color, fontWeight: FontWeight.bold),
       ),
       subtitle: Text(desc),
-      trailing: Text(
-        "R\$ ${t.balanceAfter.toStringAsFixed(2)}\n"
-        "${t.timestamp.toString().split('.')[0]}",
-      ),
+      trailing: Text("R\$ ${t.balanceAfter.toStringAsFixed(2)}\n$data"),
     );
   }
 }
