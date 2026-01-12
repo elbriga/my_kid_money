3dcb24f42893c0af9f86fe148809f304b9d73cb7
elbriga <gabrielortizlour@gmail.com>
2026-01-11 22:20:45 -0300

    remember bio

--- lib/services/biometric_service.dart
+++ lib/services/biometric_service.dart
@@ -2,16 +2,27 @@ import 'package:local_auth/local_auth.dart';
 
 class BiometricService {
   static final _disabled = 0;
+  static final _timeout = 30000; // ms
+
   static final _auth = LocalAuthentication();
+  static int _lastTSok = 0;
 
   static Future<bool> authenticate(String msg) async {
     if (_disabled == 1) return true; // Para funcionar no emulador
+
+    if (_lastTSok > 0 &&
+        (DateTime.now().millisecondsSinceEpoch - _lastTSok) < _timeout) {
+      return true;
+    }
 
     try {
       final ok = await _auth.authenticate(
         localizedReason: msg,
         biometricOnly: true,
       );
+      if (ok) {
+        _lastTSok = DateTime.now().millisecondsSinceEpoch;
+      }
       return ok;
     } catch (_) {
       return false;
